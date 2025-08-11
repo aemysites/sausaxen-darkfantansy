2794ae5d521ff039ceb9a400cf1dda0099353f06
Saurabh Saxena <sausaxen@adobe.com>
2025-08-11 22:33:37 +0530

    inject.js update

---
+++ tools/importer/transformers/inject.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* eslint-disable no-console */
+(() => {
+    const mainElement = document.querySelector('main.main.left-nav-column, main.main.all-three-column');
+    if (mainElement) {
+      mainElement.style.flexDirection = 'column';
+  
+      // make all direct children span the full width
+      Array.from(mainElement.children).forEach((child) => {
+        child.style.width = '100%';
+      });
+    }
+  
+    // remove slide-out librarian element if present
+    document.getElementById('lcs_slide_out-11923')?.remove();
+  
+    // remove offline chatwidget
+    document.querySelector('[aria-label="Chat Widget"]')?.remove();
+
+    // remove login popup container if present (e.g., on foodiesonly.in)
+    document.querySelector('.login-popup-container')?.remove();
+  
+    // remove left navigation, header, and footer elements if present
+    [
+      '.left-nav',
+      'header.header',
+      'footer.footer',
+      // FoodiesOnly specific header / footer selectors
+      '.navbar',
+      'div[class^="footer_"]', // footer element has hashed BEM/CSS-module class starting with footer_
+    ].forEach((selector) => {
+      document.querySelectorAll(selector).forEach((el) => el.remove());
+    });
+  })();
